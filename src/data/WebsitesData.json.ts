3944b07d76dfae31c7bcbcf373bb78068da7ddaa
LPdsgn <luigipedata.design@gmail.com>
2024-07-15 01:27:58 +0200

    Refined Websites and added some links in home

--- src/data/WebsitesData.json.ts
+++ src/data/WebsitesData.json.ts
@@ -3,6 +3,8 @@ export interface WebsiteData {
     mobileImageSrc: string;
     publicLink: string;
     websiteName: string;
+    tags: string[]; // Array of tags
+    online: boolean;
     details?: string; // Optional property
   }
 
@@ -12,98 +14,137 @@ export const websiteData: WebsiteData[] = [
     mobileImageSrc: "/media/websites/",
     publicLink: "https://bhblasted.com/",
     websiteName: "bhblasted.com",
-  },
+    tags: ["Showcase","Multilingual"],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/",
     mobileImageSrc: "/media/websites/",
     publicLink: "https://kaze.club/",
     websiteName: "kaze.club",
-  },
+    tags: ["Showcase",],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/whitelist-d-648caf5be5379.webp",
     mobileImageSrc: "/media/websites/whitelist-m-648caf5c281e7.webp",
     publicLink: "https://whitelist.pro/",
     websiteName: "whitelist.pro",
-    details: "Dettagli dell'elemento 2",
-  },
+    tags: ["Showcase",],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/visio-d-648caf5aedfce.webp",
     mobileImageSrc: "/media/websites/visio-m-648caf5abcbc9.webp",
     publicLink: "https://www.visiodp.com/",
     websiteName: "visiodp.com",
-  },
+    tags: ["Showcase","Portfolio","Multilingual"],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/",
     mobileImageSrc: "/media/websites/",
     publicLink: "https://facetraining.it/",
     websiteName: "facetraining.it",
-  },
+    tags: ["eCommerce"],
+  online: true,
+},
+  {
+    desktopImageSrc: "/media/websites/",
+    mobileImageSrc: "/media/websites/",
+    publicLink: "https://tekneav.com/",
+    websiteName: "tekneav.com",
+    tags: ["Showcase",],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/guerritore-d-648caf50ddf1b.webp",
     mobileImageSrc: "/media/websites/guerritore-m-648caf50d6826.webp",
     publicLink: "https://viniguerritore.it/",
     websiteName: "viniguerritore.it",
-  },
+    tags: ["Showcase","Multilingual"],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/monsterhouse-d-648caf551b345.webp",
     mobileImageSrc: "/media/websites/monsterhouse-m-648caf5576acd.webp",
     publicLink: "https://monsterhouse.ch/",
     websiteName: "monsterhouse.ch",
-  },
+    tags: ["Showcase","Portfolio"],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/belcuore-d-648caf4d193d6.webp",
     mobileImageSrc: "/media/websites/belcuore-m-648caf4d6acad.webp",
     publicLink: "https://studiobelcuore.it/",
     websiteName: "studiobelcuore.it",
-  },
+    tags: ["Showcase","Portfolio"],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/mtvcamp-d-648caf5816101.webp",
     mobileImageSrc: "/media/websites/mtvcamp-m-648caf58a9bfc.webp",
     publicLink: "https://mtvcampania.org",
     websiteName: "mtvcampania.org",
-  },
+    tags: ["Listings","Multilingual"],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/h2power-d-648caf521fbba.webp",
     mobileImageSrc: "/media/websites/h2power-m-648caf523d0d6.webp",
     publicLink: "https://h2powersrl.it/",
     websiteName: "h2powersrl.it",
-  },
-  /* {
+    tags: ["Showcase",],
+  online: true,
+},
+  {
     desktopImageSrc: "/media/websites/plpharma-d-648caf59b56a5.webp",
     mobileImageSrc: "/media/websites/plpharma-m-648caf59a86cc.webp",
     publicLink: "https://plpharma.it/",
     websiteName: "plpharma.it",
-  }, */
-  /* {
+    tags: ["eCommerce"],
+  online: false,
+},
+  {
     desktopImageSrc: "/media/websites/mpmeccanica-m-648caf5732ca8.webp",
     mobileImageSrc: "/media/websites/mpmeccanica-d-648caf567d5fb.webp",
     publicLink: "https://mpmeccanica.com/",
     websiteName: "mpmeccanica.com",
-  }, */
+    tags: ["Showcase","Multilingual"],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/ils-m-648caf5365679.webp",
     mobileImageSrc: "/media/websites/ils-d-648caf5336d7e.webp",
     publicLink: "https://ilsformazione.it/",
     websiteName: "ilsformazione.it",
-  },
+    tags: ["Showcase",],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/coohesion-d-648caf4fa2f92.webp",
     mobileImageSrc: "/media/websites/coohesion-m-648caf4fc4cdd.webp",
     publicLink: "https://coohesion.com/",
     websiteName: "coohesion.com",
-  },
+    tags: ["eCommerce","Multilingual"],
+  online: true,
+},
   {
     desktopImageSrc: "/media/websites/allprint-d-648caf4c06422.webp",
     mobileImageSrc: "/media/websites/allprint-m-648caf4beae38.webp",
     publicLink: "https://www.allprintpaper.it/",
     websiteName: "allprintpaper.it",
-  },
-  /* {
+    tags: ["Showcase"],
+  online: true,
+},
+  {
     desktopImageSrc: "/media/websites/belcuoresrl-d-648caf4e7b7dc.webp",
     mobileImageSrc: "/media/websites/belcuoresrl-m-648caf4e63c3f.webp",
     publicLink: "https://www.francescobelcuoresrl.com/",
     websiteName: "belcuoresrl.com",
-  }, */
+    tags: ["Showcase","Portfolio"],
+  online: true,
+},
 
   // Add more elements as needed
   
