b5ab2513027019e414736c5bfe8c6b5249427ced
LPdsgn <luigipedata.design@gmail.com>
2024-09-15 18:44:30 +0200

    Removed Partytown dependecy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from '@astrojs/react';
 //import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
 import vercel from '@astrojs/vercel/serverless';
-// import partytown from "@astrojs/partytown";
 
 // Dev Toolbar extensions
 import metaTags from "astro-meta-tags";
@@ -23,7 +22,7 @@ export default defineConfig({
 
   integrations: [tailwind({
     applyBaseStyles: false,
-  }), alpinejs(), react(), metaTags(), pageInsight(), vtbot(), tailwindConfigViewer(), partytown()],
+  }), alpinejs(), react(), metaTags(), pageInsight(), vtbot(), tailwindConfigViewer()],
 
   output: "server",
   adapter: vercel({
